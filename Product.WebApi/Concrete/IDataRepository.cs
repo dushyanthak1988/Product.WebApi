b110fa45763664e89d792f15a02c9dca78d44ad6
Dushyantha Kalehewatte <dushyanthak@outlook.com>
2023-06-28 22:55:24 +0530

    add Employee CRUD operation

---
+++ Product.WebApi/Concrete/IDataRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Product.WebApi.Concrete
+{
+    public interface IDataRepository<TEntity>
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(long id);
+        void Add(TEntity entity);
+        void Update(TEntity dbEntity, TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ Product.WebApi/Service/EmployeeManager.cs
@@ -0,0 +1,43 @@
+﻿using Product.WebApi.Concrete;
+using Product.WebApi.Data;
+using Product.WebApi.Models;
+
+namespace Product.WebApi.Service
+{
+    public class EmployeeManager : IDataRepository<Employee>
+    {
+        readonly ApplicationDbContext _employeeContext;
+        public EmployeeManager(ApplicationDbContext context)
+        {
+            _employeeContext = context;
+        }
+        public IEnumerable<Employee> GetAll()
+        {
+            return _employeeContext.Employees.ToList();
+        }
+        public Employee Get(long id)
+        {
+            return _employeeContext.Employees
+                  .FirstOrDefault(e => e.EmployeeId == id);
+        }
+        public void Add(Employee entity)
+        {
+            _employeeContext.Employees.Add(entity);
+            _employeeContext.SaveChanges();
+        }
+        public void Update(Employee employee, Employee entity)
+        {
+            employee.FirstName = entity.FirstName;
+            employee.LastName = entity.LastName;
+            employee.Email = entity.Email;
+            employee.DateOfBirth = entity.DateOfBirth;
+            employee.PhoneNumber = entity.PhoneNumber;
+            _employeeContext.SaveChanges();
+        }
+        public void Delete(Employee employee)
+        {
+            _employeeContext.Employees.Remove(employee);
+            _employeeContext.SaveChanges();
+        }
+    }
+}
